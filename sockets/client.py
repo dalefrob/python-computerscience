84ed13799954d8f703203d2c2ebfa611da64bf40
dalefrob <dalef.rob@gmail.com>
2025-05-07 23:53:43 +0900

    networking & car split screen

--- sockets/client.py
+++ sockets/client.py
@@ -1,7 +1,25 @@
 import socket
 
-client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client.connect(('127.0.0.1', 9999))
+HEADER = 64 # Tells the server how big the incoming message is
+PORT = 9999
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
+SERVER = "192.168.0.237"
+ADDR = (SERVER, PORT)
 
-client.send("Hello from client!".encode())
-print(client.recv(1024).decode())
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(ADDR)
+
+def send(msg):
+  message = msg.encode(FORMAT)
+  msg_length = len(message)
+  send_length = str(msg_length).encode(FORMAT)
+  send_length += b' ' * (HEADER - len(send_length))
+  client.send(send_length)
+  client.send(message)
+  print(client.recv(2048).decode(FORMAT))
+
+
+send("Hello World!")
+send("Hello cheapos!")
+send(DISCONNECT_MESSAGE)
--- sockets/server.py
+++ sockets/server.py
@@ -1,11 +1,44 @@
 import socket
+import threading
+
+HEADER = 64 # Tells the server how big the incoming message is
+PORT = 9999
+SERVER = socket.gethostbyname(socket.gethostname())
+ADDR = (SERVER, PORT)
+FORMAT = 'utf-8'
+DISCONNECT_MESSAGE = "!DISCONNECT"
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('0.0.0.0', 9999))
+server.bind(ADDR)
 
-server.listen(5)
 
-while True:
-  client, addr = server.accept()
-  print(client.recv(1024).decode())
-  client.send('Hello from server'.encode())
+def handle_client(conn, addr):
+  print(f"[NEW CONNECTION] {addr} connected.")
+
+  connected = True
+  while connected:
+    msg_length = conn.recv(HEADER).decode(FORMAT) # Blocking, therefore it should run in a thread
+    if msg_length:
+      msg_length = int(msg_length) # Cast to int
+      msg = conn.recv(msg_length).decode(FORMAT)
+      if msg == DISCONNECT_MESSAGE:
+        connected = False
+      print(f"[{addr}] {msg}")
+      conn.send("Message received".encode(FORMAT))
+  
+  conn.close() # Cleanly disconnect
+
+
+def start():
+  server.listen()
+  print(f"[LISTENING] Server is listening on {SERVER}")
+  while True:
+    conn, addr = server.accept() # Blocks execution until a connection occurs
+    thread = threading.Thread(target=handle_client, args=(conn, addr))
+    thread.start()
+    print(f"[ACTIVE CONNECTIONS] {threading.active_count() -1}")
+
+
+print("Starting Server")
+start()
+
