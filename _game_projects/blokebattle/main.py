d6aa69772b97b7c2bcb88d7f229df66bbdb275b3
dalefrob <dalef.rob@gmail.com>
2025-06-23 15:40:57 +0900

    bb
    added regular members

--- _game_projects/blokebattle/main.py
+++ _game_projects/blokebattle/main.py
@@ -7,24 +7,26 @@ print("Loading Bloke Battle...\n")
 def gen_test_blokes():
     blokes = []
 
-    brad = Bloke("Brad")
-    brad.equip_item(make_weapon())
-    blokes.append(brad)
-    
-    blokes.append(Bloke("Rob"))
+    boa = Bloke("Boa")
+    boa.equip_item(make_weapon())
+    blokes.append(boa)
 
-    carter = Bloke("Carter")
+    mimi = Bloke("Mimi")
+    mimi.equip_item(make_healing_weapon())
+    blokes.append(mimi)
+    
+
+    carter = Bloke("Ethan")
     carter.equip_item(make_fire_weapon())
     blokes.append(carter)
 
-    dale = Bloke("Dale")
+    dale = Bloke("David")
     dale.equip_item(make_ring())
     blokes.append(dale)
 
-    orion = Bloke("Orion")
-    orion.spells.append(("doomsday", 15))
-    orion.buff_manager.add_buff(make_reflect())
-    blokes.append(orion)
+    cindy = Bloke("Cindy")
+    cindy.buff_manager.add_buff(make_reflect())
+    blokes.append(cindy)
 
     return blokes
 
