a2c7fcd40445faaefeba57603c2148fbfa56ea0e
dalefrob <dalef.rob@gmail.com>
2025-07-15 15:26:33 +0900

    saturday lesson

--- lessons/dcrawler/dcrawler.py
+++ lessons/dcrawler/dcrawler.py
@@ -7,23 +7,60 @@ def print_map(map, player):
         rowstr = ""
         for x in range(len(map[y])):
             roomstr = "[ ]"
+            if map[x][y] == None:
+                roomstr = "   "
             if player.x == x and player.y == y:
                 roomstr = "[P]"
-            rowstr += f"({x},{y}) {roomstr}"
+            rowstr += f"{roomstr}"
         print(rowstr)
 
 def main():
     # Initialize the dungeon map with room objects
     dmap = [
         [Room("A dusty old classroom"), Room("A room with cracked walls"), None],
-        [Room("The haunted bathroom"), Room("The bedroom"), Room("The slippery stairs")],
+        [None, Room("The bedroom"), Room("The slippery stairs")],
         [Room("The spooky backyard"), Room("The well"), Room("The foggy forest")]
     ]
     # Create the player object
     player = Player(0,0)
     print_map(dmap, player)
-    player.move(1, 0, dmap) # Move the player --> in x
-    print_map(dmap, player)
+    # TODO - Make a basic game loop.
+    gameover = False
+    while not gameover:
+        command = input("Enter command (exit, N, E, S, W): ")
+        did_move = False
+        match command:
+            case "exit":
+                gameover = True
+                break
+            case "S":
+                did_move = player.move(0, 1, dmap)
+                print_map(dmap, player)
+            case "E":
+                did_move = player.move(1, 0, dmap)
+                print_map(dmap, player)
+            case "N":
+                did_move = player.move(0, -1, dmap)
+                print_map(dmap, player)
+            case "W":
+                did_move = player.move(-1, 0, dmap)
+                print_map(dmap, player)
+
+        if did_move == True:
+            print("Player Moved")
+            current_room : Room = dmap[player.x][player.y]
+            print(current_room.description)
+        else:
+            print("Could not move!")
+
+    # While not gameover:
+    #   Show possible commands - north, east, south, west, look, take, map
+    #   Get player input, and validate it
+    #   Act on input
+    #   If player is in exit room:
+    #       Break loop wth YOU WIN
+    #   If player dies in a trap:
+    #       Break loop with GAME OVER
 
 # Entry point into the program
 if __name__ == "__main__":
--- lessons/dcrawler/player.py
+++ lessons/dcrawler/player.py
@@ -4,6 +4,18 @@ class Player:
         self.y = y
         self.inventory = []
     
-    def move(self, x_dir : int, y_dir : int, map):
-        self.x += x_dir
-        self.y += y_dir
+    def move(self, x_dir : int, y_dir : int, map) -> bool:
+        new_x = self.x + x_dir
+        new_y = self.y + y_dir
+
+        if new_x < 0 or new_x >= len(map[0]):
+            return False
+        if new_y < 0 or new_y >= len(map):
+            return False
+        if map[new_x][new_y] == None:
+            return False
+
+        self.x = new_x
+        self.y = new_y
+
+        return True
